cf9874fae99df2c22f911ee3fb823f2af3b44e7c
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-09 19:02:38 +0900

    createアクションの編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,12 +7,21 @@ class ProductsController < ApplicationController
   end
 
   def create
+    def create
+      @product = Prototype.new(product_params)
+      @product.save
+      if @product.save
+        redirect_to root_path
+      else
+         render :new
+      end
+    end  
   end
 
 
   private
 
   def product_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:title, :explanation, :price, :image).merge(user_id: current_user.id)
   end
 end
