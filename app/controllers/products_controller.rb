20e8430d62b0180a65189557abbb1560fe687bfb
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-09 17:28:01 +0900

    画像の保存を許可するストロングパラメーターを定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,17 @@
 class ProductsController < ApplicationController
-  def index
-    
+  def index  
   end
   
   def new
   end
 
   def create
+  end
+
+
+  private
+
+  def product_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
