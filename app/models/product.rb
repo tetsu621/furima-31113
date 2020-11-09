9a97623c88e92a22c85fc3b8342b728651376ef4
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-09 16:38:37 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
+  validates :title, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :area_id, presence: true
+  validates :days_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
