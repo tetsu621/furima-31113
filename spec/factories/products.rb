e7f8613a36225d11bd2476a0232af5b1e38271b0
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-11 18:37:20 +0900

    文字列を整数に変更

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :product do
     title {"sample"}
     explanation {"sample"}
-    category_id {"1"}
-    status_id {"1"}
-    delivery_fee_id {"1"}
-    area_id {"2"}
-    day_id {"1"}
-    price {"400"}
+    category_id {1}
+    status_id {1}
+    delivery_fee_id {1}
+    area_id {2}
+    day_id {1}
+    price {400}
     association :user
 
     after(:build) do |product|
