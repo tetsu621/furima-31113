87e25e04bcb94b5a77926d5e94b9c8fa4788933c
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-12 18:42:26 +0900

    ログインしていないユーザーも詳細画面に遷移することを許可

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   def index  
     @products = Product.includes(:user).order("created_at DESC")
   end
