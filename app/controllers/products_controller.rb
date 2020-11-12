b8dd1b97c18352c997097532e9d6bc039eb4ea28
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-12 16:48:51 +0900

    表示順の並び替えとN+1問題の解消

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index  
-    @products = Product.all
+    @products = Product.includes(:user).order("created_at DESC")
   end
   
   def new
