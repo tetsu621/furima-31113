f437ff044fa8b9ff900d1b05a33408c2577920ee
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-14 03:05:17 +0900

    フォームオブジェクトの編集とモデルの編集

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,11 +12,9 @@ class OrderAddress
    end 
 
   def save
-    # ユーザーの情報を保存し、「user」という変数に入れている
-    user = User.create(name: name, name_reading: name_reading, nickname: nickname)
     # 住所の情報を保存
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name,user_id: user.id)
+    Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name,user_id: user.id)
     # 寄付金の情報を保存
-    Donation.create(price: price, user_id: user.id)
+    Order.create(product: product, user_id: user.id)
   end
 end
