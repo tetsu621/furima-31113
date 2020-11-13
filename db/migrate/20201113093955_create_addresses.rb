64a73c55ff53ce8fd6607e4c3755935afce7d475
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-13 18:49:19 +0900

    マイグレーションの設定

--- db/migrate/20201113093955_create_addresses.rb
+++ db/migrate/20201113093955_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string     :postal_code,    null: false
+      t.integer    :prefectures_id, null: false
+      t.string     :municipality,   null: false
+      t.string     :address,        null: false
+      t.string     :building_name,
+      t.string     :phone_number,   null: false
+      t.references :order,          null: false
       t.timestamps
     end
   end
