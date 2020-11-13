1fe69fa46088f8d16ae565a4425883199760c806
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-13 21:25:05 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:new, :create, :index, :show] 
-  resources :orders
+  resources :products, only: [:new, :create, :index, :show] do
+    resources :orders, only: [:index, :create]
+  end
+    
 end
