9fd8470a196028d9876aa5040796e249fa3ae906
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-14 10:53:39 +0900

    formオブジェクトを整理

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number
+  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :product_id, :token
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
@@ -12,9 +12,9 @@ class OrderAddress
    end 
 
   def save
-    # 住所の情報を保存
-    Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name,user_id: user.id)
-    # 寄付金の情報を保存
-    Order.create(product: product, user_id: user.id)
+    product = Product.find(product.id)
+    user = product.user   
+    address = Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name,order_id: order.id)
+    order = Order.create(product_id: product_id, user_id: user.id, token: token)
   end
 end
