9c2a9dc7dfaa6ea56e6d7ce33b0217fa6876fa5e
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-14 12:21:12 +0900

    コントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,24 @@ class OrdersController < ApplicationController
   def index
     @product = Product.find(params[:product_id])
   end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @oreder_address.valid
+      pay_product
+      @order_address.save
+      redirect_to root_path
+    else
+     @product = prodoct.find(params[:product_id])
+     render action: :index
+    end 
+  end
+
+  private
+
+    def order_params
+      params.require(:order_address).permit(:postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number).merge(product_id: params[product_id])
+    end
+
 end
+
