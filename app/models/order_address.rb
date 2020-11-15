6d1a80817d90857b1b932266a13102c88d30eeb0
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-15 19:27:38 +0900

    バリデーションの再設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,20 +1,19 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :product_id, :token
+  attr_accessor :postal_code, :area_id, :municipality, :address, :building_name, :phone_number, :product_id, :user_id, :token
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
-    validates :postal_code
-    validates :prefectures_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :municipality
     validates :address
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{1,11}\z/ }
    end 
 
+  validates :area_id, numericality: { other_than: 1}
+
   def save
-    product = Product.find(product.id)
-    user = product.user   
-    address = Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name,order_id: order.id)
-    order = Order.create(product_id: product_id, user_id: user.id, token: token)
+    address = Address.create(postal_code: postal_code, area_id: area_id, municipality: municipality, address: address, building_name: building_name, order_id: order.id)
+    order = Order.create(product_id: product_id, user_id: user.id)
   end
 end
