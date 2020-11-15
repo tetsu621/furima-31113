ffcacd941f8df73f632790d2b5dc51eefd743409
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-15 21:54:35 +0900

    payjpの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,7 @@ class OrdersController < ApplicationController
   end
 
   def create
+    binding.pry
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       #pay_product
