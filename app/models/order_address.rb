8089ef3aa1519470e809350a29bb413f4777fde0
tetsu621 <tetsu0621tkg@gmail.com>
2020-11-16 20:44:37 +0900

    createメソッドに変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,7 +14,7 @@ class OrderAddress
   validates :area_id, numericality: { other_than: 1}
 
   def save
-    order = Order.create!(product_id: product_id, user_id: user_id)
-    Address.create!(postal_code: postal_code, area_id: area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    order = Order.create(product_id: product_id, user_id: user_id)
+    Address.create(postal_code: postal_code, area_id: area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
